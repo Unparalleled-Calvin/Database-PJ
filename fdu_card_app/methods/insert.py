a9cb3f66329cf9520cc59c5f4692ec350aca140c
Nemo <2321848845@qq.com>
2021-04-26 16:44:00 +0800

    完善数据库建立，完成插入函数

---
+++ fdu_card_app/methods/insert.py
@@ -0,0 +1,81 @@
+def insert_person(cursor, id, name):
+    sql = "insert into person values('{}', '{}')".format(id, name)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of person.")
+
+
+def insert_teacher_ranknull(cursor, id, age):
+    sql = "insert into teacher values('{}', '{}')".format(
+        id, age)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of teacher.")
+
+
+def insert_teacher(cursor, id, age, rank):
+    sql = "insert into teacher values('{}', '{}', '{}')".format(
+        id, age, rank)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of teacher.")
+
+
+def insert_student(cursor, id, enrolmentdt, class):
+    sql = "insert into student values('{}', '{}', '{}')".format(
+        id, enrolmentdt, class)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of student.")
+
+
+def insert_others(cursor, id, work):
+    sql = "insert into others values('{}', '{}')".format(
+        id, work)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of others.")
+
+
+def insert_domitory(cursor, dno, dadmin, dtel, dfloor):
+    sql = "insert into domitory values('{}', '{}', '{}', '{}')".format(
+        dno, dadmin, dtel, dfloor)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of domitory.")
+
+
+def insert_card(cursor, id, remainingsum=0, carddate='now()', cdno='null', valid=0):
+    sql = "insert into card values('{}', '{}', '{}', {}, '{}')".format(
+        id, remainingsum, carddate, cdno, valid)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of card.")
+
+
+def insert_canteen(cursor, wno, wname, wadmin, wtel):
+    sql = "insert into canteen values('{}', '{}', '{}', '{}')".format(
+        wno, wname, wadmin, wtel)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of canteen.")
+
+
+def insert_gate(cursor, gno, gname, gadmin, gtel):
+    sql = "insert into gate values('{}', '{}', '{}', '{}')".format(
+        gno, gname, gadmin, gtel)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of gate.")
+
+
+def insert_consume(cursor, wno, ID, cuisineid, amount):
+    sql = "call eat('{}', '{}', '{}', '{}')".format(
+        ID, wno, cuisineid, amount)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of consume.")
+
+
+def insert_record(cursor, ID, gno, inout):
+    sql = "call in_and_out('{}', '{}', '{}')".format(
+        ID, gno, inout)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of record.")
+
+
+def insert_access(cursor, ID, dno):
+    sql = "call in_and_out('{}', '{}')".format(
+        ID, dno)
+    cursor.execute(sql)
+    print("Success! You have added a member to the table of access.")
--- files/create.sql
+++ files/create.sql
@@ -1,36 +1,36 @@
 create TABLE person(
-    ID VARCHAR(10) PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    check(length(ID) in (5, 6, 10))
+    ID VARCHAR(11) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    check(length(ID) in (5, 6, 11))
 );
 create TABLE teacher(
-    ID VARCHAR(10) PRIMARY KEY,
+    ID VARCHAR(11) PRIMARY KEY,
     age int NOT NULL,
     rank VARCHAR(20),
     foreign key(ID) REFERENCES person(ID) ON DELETE CASCADE ON UPDATE CASCADE,
     check(length(ID) = 6)
 );
 create TABLE student(
-    ID VARCHAR(10) PRIMARY KEY,
+    ID VARCHAR(11) PRIMARY KEY,
     enrolmentdt DATE NOT NULL,
     class VARCHAR(20) NOT NULL,
     foreign key(ID) REFERENCES person(ID) ON DELETE CASCADE ON UPDATE CASCADE,
-    check(length(ID) = 10)
+    check(length(ID) = 11)
 );
 create TABLE others(
-    ID VARCHAR(10) PRIMARY KEY,
+    ID VARCHAR(11) PRIMARY KEY,
     work VARCHAR(20) NOT NULL,
     foreign key(ID) REFERENCES person(ID) ON DELETE CASCADE ON UPDATE CASCADE,
     check(length(ID) = 5)
 );
 create TABLE domitory(
     dno INT PRIMARY KEY,
-    dadmin VARCHAR(10) NOT NULL,
+    dadmin VARCHAR(50) NOT NULL,
     dtel VARCHAR(11) NOT NULL,
     dfloor INT NOT NULL
 );
 create TABLE card(
-    ID VARCHAR(10),
+    ID VARCHAR(11),
     remainingsum NUMERIC(10, 2) DEFAULT 0,
     carddate TIMESTAMP DEFAULT now(),
     cdno INT DEFAULT null,
@@ -42,19 +42,19 @@ create TABLE card(
 );
 create TABLE canteen(
     wno INT PRIMARY KEY,
-    wname VARCHAR(20) NOT NULL,
-    wadmin VARCHAR(10) NOT NULL,
+    wname VARCHAR(50) NOT NULL,
+    wadmin VARCHAR(50) NOT NULL,
     wtel VARCHAR(11) NOT NULL
 );
 create TABLE gate(
     gno INT PRIMARY KEY,
-    gname VARCHAR(20) NOT NULL,
-    gadmin VARCHAR(10) NOT NULL,
+    gname VARCHAR(50) NOT NULL,
+    gadmin VARCHAR(50) NOT NULL,
     gtel VARCHAR(11) NOT NULL
 );
 create TABLE consume(
     wno INT NOT NULL,
-    ID VARCHAR(10) NOT NULL,
+    ID VARCHAR(11) NOT NULL,
     consumetm TIMESTAMP DEFAULT now(),
     cuisineid INT NOT NULL,
     amount numeric(10, 2) NOT NULL,
@@ -63,7 +63,7 @@ create TABLE consume(
     foreign key(wno) REFERENCES canteen(wno) ON DELETE CASCADE ON UPDATE CASCADE
 );
 create TABLE record(
-    ID VARCHAR(10) NOT NULL,
+    ID VARCHAR(11) NOT NULL,
     gno INT NOT NULL,
     recordtm TIMESTAMP DEFAULT now(),
     inout VARCHAR(3) NOT NULL,
@@ -73,7 +73,7 @@ create TABLE record(
     check(inout in('in', 'out'))
 );
 create TABLE access(
-    ID VARCHAR(10) NOT NULL,
+    ID VARCHAR(11) NOT NULL,
     dno int NOT NULL,
     accesstm TIMESTAMP DEFAULT now(),
     PRIMARY KEY(ID, dno, accesstm),
@@ -115,7 +115,7 @@ create view v_access as(
         natural join access
     where valid = 1
 );
-create or replace function charge(fID varchar(10), famount numeric(10, 2)) returns numeric as $$ begin
+create or replace function charge(fID varchar(11), famount numeric(10, 2)) returns numeric as $$ begin
 update card
 set remainingsum = remainingsum + famount
 where fID = card.ID
@@ -129,13 +129,14 @@ return (
 end;
 $$ language plpgsql;
 create or replace procedure eat(
-        in pID varchar(10),
+        in pID varchar(11),
         in pwno int,
         in pcuisineid int,
         in pamount numeric(10, 2)
     ) as $$ begin if pID not in(
         select ID
         from card
+        where valid = 1
     ) then raise notice 'Failed. The ID is error.';
 elsif pwno not in(
     select wno
@@ -150,6 +151,56 @@ elsif pamount > (
 else
 insert into consume(wno, ID, cuisineid, amount)
 values(pwno, pID, pcuisineid, pamount);
+end if;
+end;
+$$ language plpgsql;
+create or replace procedure in_and_out(
+        in pID varchar(11),
+        in pgno int,
+        in pinout varchar(3)
+    ) as $$ begin if pID not in(
+        select ID
+        from card
+        where valid = 1
+    ) then raise notice 'Failed. The ID is error.';
+elsif pgno not in(
+    select gno
+    from gate
+) then raise notice 'Failed. The gno is error.';
+elsif pinout not in('in', 'out') then raise notice 'Failed. The inout is wrong.';
+else
+insert into record(ID, gno, inout)
+values(pID, pgno, pinout);
+end if;
+end;
+$$ language plpgsql;
+create or replace procedure back(
+        in pID varchar(11),
+        in pdno int
+    ) as $$ begin if pID not in(
+        select ID
+        from card
+        where valid = 1
+    ) then raise notice 'Failed. The ID is error.';
+elsif pdno not in(
+    select dno
+    from domitory
+) then raise notice 'Failed. The dno is error.';
+elsif pdno not in(
+    select cdno
+    from card
+    where card.ID = pID
+        and card.valid = 1
+)
+or (
+    select cdno
+    from card
+    where card.ID = pID
+        and card.valid = 1
+) is null then raise notice 'Failed. The ID can not access to the gno.';
+else
+insert into access(ID, dno)
+values(pID, pdno);
 end if;
 end;
 $$ language plpgsql;
