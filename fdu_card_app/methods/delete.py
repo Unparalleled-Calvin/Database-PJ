e151cb66c116ebbc22e56da07e417000aaef0943
Nemo <2321848845@qq.com>
2021-05-14 20:31:58 +0800

    delete完成

---
+++ fdu_card_app/methods/delete.py
@@ -0,0 +1,26 @@
+# 删除人员信息
+def delete_person(cursor, id):
+    sql = "delete from person where ID = '{}'".format(id)
+    cursor.execute(sql)
+    return cursor.rowcount
+
+
+# 删除宿舍
+def delete_domitory(cursor, dno):
+    sql = "delete from domitory where dno = '{}'".format(dno)
+    cursor.execute(sql)
+    return cursor.rowcount
+
+
+# 删除餐厅
+def delete_canteen(cursor, wno):
+    sql = "delete from canteen where wno = '{}'".format(wno)
+    cursor.execute(sql)
+    return cursor.rowcount
+
+
+# 删除校门
+def delete_gate(cursor, gno):
+    sql = "delete from gate where gno = '{}'".format(gno)
+    cursor.execute(sql)
+    return cursor.rowcount
