3fe34368df2e7db774d065bcaa4dd5c559c66548
Calvin <19307130084@fudan.edu.cn>
2021-05-29 18:39:17 +0800

    实现了窗口元素位置监听改变侧边栏行为

--- fdu_card_app/static/js/gargets.js
+++ fdu_card_app/static/js/gargets.js
@@ -86,3 +86,40 @@ function showTable(tableId, data) {
     var table = document.getElementById(tableId);
     table.innerHTML = tableHTML;
 }
+
+jQuery.fn.onPositionChanged = function (trigger, millis) {
+    if (millis == null) millis = 100;
+    var o = $(this[0]); // our jquery object
+    if (o.length < 1) return o;
+
+    var lastPos = null;
+    var lastOff = null;
+    setInterval(function () {
+        if (o == null || o.length < 1) return o; // abort if element is non existend eny more
+        if (lastPos == null) lastPos = o.position();
+        if (lastOff == null) lastOff = o.offset();
+        var newPos = o.position();
+        var newOff = o.offset();
+        if (lastPos.top != newPos.top || lastPos.left != newPos.left) {
+            $(this).trigger('onPositionChanged', { lastPos: lastPos, newPos: newPos });
+            if (typeof (trigger) == "function") trigger(lastPos, newPos);
+            lastPos = o.position();
+        }
+        if (lastOff.top != newOff.top || lastOff.left != newOff.left) {
+            $(this).trigger('onOffsetChanged', { lastOff: lastOff, newOff: newOff });
+            if (typeof (trigger) == "function") trigger(lastOff, newOff);
+            lastOff = o.offset();
+        }
+    }, millis);
+
+    return o;
+};
+function fitWindow() {
+    var section_header_rec = $(".section-header")[0].getBoundingClientRect();
+    var sidebar_wrapper_rec = $("#sidebar-wrapper")[0].getBoundingClientRect();
+    var body = $('body')
+    if (section_header_rec.left <= sidebar_wrapper_rec.right) {
+        body.removeClass('sidebar-show');
+        body.addClass('sidebar-mini');
+    }
+}
